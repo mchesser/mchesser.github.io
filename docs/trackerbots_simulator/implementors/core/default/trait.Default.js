660cb27d389664d4e73a3df901c677bf538afd77
Michael Chesser <michaelchesser@outlook.com>
2016-09-25 16:55:00 +0930

    Add docs to page.

---
+++ docs/trackerbots_simulator/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["simulation"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='simulation/struct.SimulationConfig.html' title='simulation::SimulationConfig'>SimulationConfig</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
